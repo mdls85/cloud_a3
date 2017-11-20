8dbab3d1a7562d734c7738eb7e768bf71b13a291
logan20 <kwasi_edwards2@hotmail.com>
2017-11-20 16:59:16 -0400

    final working receiver

--- setup_worker_vm.sh
+++ setup_worker_vm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone https://github.com/mdls85/cloud_a3.git
-cd cloud_a3
 
-sh pullfrom_queue.sh
+git clone https://github.com/mdls85/cloud_a3.git
+
+java -jar /var/lib/waagent/Microsoft.OSTCExtensions.CustomScriptForLinux-1.5.2.2/download/0/cloud_a3/Receiver/CloudAssignment3.jar
