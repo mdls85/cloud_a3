304fcf2ea3fcf088c4a417fa68fa8fb05645b03f
logan20 <kwasi_edwards2@hotmail.com>
2017-11-20 18:27:17 -0400

    last lap

--- setup_worker_vm.sh
+++ setup_worker_vm.sh
@@ -4,3 +4,4 @@
 git clone https://github.com/mdls85/cloud_a3.git
 
 java -jar /var/lib/waagent/Microsoft.OSTCExtensions.CustomScriptForLinux-1.5.2.2/download/0/cloud_a3/Receiver/CloudAssignment3.jar
+
