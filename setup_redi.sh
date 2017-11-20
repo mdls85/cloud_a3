0d07b0c44f6971bfe049c888614086b72ee2446d
Matthew <mdls85@hotmail.com>
2017-11-20 16:22:20 -0400

    modified bash script to setup redis

--- setup_redi.sh
+++ setup_redi.sh
@@ -9,4 +9,4 @@ cd redis-4.0.2
 make
 
 wget https://raw.githubusercontent.com/mdls85/cloud_a3/master/redis.conf
-src/redis-server /home/logan20/redis-4.0.2/redis.conf.1
+src/redis-server /var/lib/waagent/Microsoft.OSTCExtensions.CustomScriptForLinux-1.5.2.2/download/0/redis-4.0.2/redis.conf.1
