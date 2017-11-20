9e28530cea418e7ef39340afe87e31c5475f9696
root <root@localhost.localdomain>
2017-11-19 22:37:10 -0400

    attempting script but litle success

--- template_script.sh
+++ template_script.sh
@@ -2,6 +2,12 @@
 
 resourceGroupName="HighMarksTrial"
 location="westus2"
+virtualMachineScaleSetName="HighMarksVMSS"
+imageName="UbuntuLTS"
+autoscaleName="AutoScale"
+autoscaleParametersFile="autoscaleparams.txt"
+servicebusName="HighMarksServicebus"
+serviebusQueueName="HighMarksSBQueue"
 
 #install the azure cli
 #curl -L https://aka.ms/InstallAzureCli | bash  <--UNCOMMENT 
@@ -14,12 +20,15 @@ location="westus2"
 echo "Please login to azure!"
 #az login   <--UNCOMMENT 
 
+
 #Create the resource group where all the things will be
-#az group create --name $resourceGroupName --location $location   <--UNCOMMENT 
+#az group create --name $resourceGroupName --location $location 
 
 
-#Create the queue where the items will be stored
-echo "Creating the queue so to store the items that the generator will store"
+#Create the service bus and queue where the items will be stored
+echo "Creating the service bus and queue to store the items that the generator will store"
+az group deployment create --resource-group $resourceGroupName -n $servicebusName --template-uri https://raw.githubusercontent.com/azure/azure-quickstart-templates/master/201-servicebus-create-queue/azuredeploy.json
+
 
 
 #Create the VM to do the 1M requests
@@ -29,5 +38,8 @@ echo "Creating the virtual machine to process the 1 million requests"
 
 #Create the Virtual Scale Set to do the auto scaling
 echo "Creating the virtual scale set to process items from the queue"
+#az vmss create --image $imageName --name $virtualMachineScaleSetName --resource-group $resourceGroupName
 
+
+#Create autoscale set rules for the virtual set
 
