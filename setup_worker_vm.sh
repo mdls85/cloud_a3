140a93f025d8b0c20fc75b5983331eee67af1e28
Matthew <mdls85@hotmail.com>
2017-11-20 23:38:09 -0400

    mod java installation in bash script for worker

--- setup_worker_vm.sh
+++ setup_worker_vm.sh
@@ -3,7 +3,7 @@
 
 git clone https://github.com/mdls85/cloud_a3.git
 
-sudo apt -y install default-jre
+sudo apt-get -y install default-jre
 
 java -jar /var/lib/waagent/Microsoft.OSTCExtensions.CustomScriptForLinux-1.5.2.2/download/0/cloud_a3/Receiver/CloudAssignment3.jar
 
