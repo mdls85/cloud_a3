4646f8298fcac6eef2358ab5af60224ed52d3440
logan20 <kwasi_edwards2@hotmail.com>
2017-11-21 00:14:14 -0400

    Changes so that the loader vm will hit the queue with 1000000 requests

--- bombard_queue.sh
+++ bombard_queue.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-numRequests=1000
-hatchRate=100
-maxSimultaneousClients=10
+numRequests=1000000
+hatchRate=20
+maxSimultaneousClients=100
 
 locust -f load_gen.py --no-web --only-summary --num-request=$numRequests --hatch-rate=$hatchRate --clients=$maxSimultaneousClients
