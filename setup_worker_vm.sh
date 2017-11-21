4dc2d8ef2742a2564fbec6161e36eeea67a01a56
Matthew <mdls85@hotmail.com>
2017-11-20 23:40:13 -0400

    mod path in bash script for worker

--- setup_worker_vm.sh
+++ setup_worker_vm.sh
@@ -5,5 +5,5 @@ git clone https://github.com/mdls85/cloud_a3.git
 
 sudo apt-get -y install default-jre
 
-java -jar /var/lib/waagent/Microsoft.OSTCExtensions.CustomScriptForLinux-1.5.2.2/download/0/cloud_a3/Receiver/CloudAssignment3.jar
+java -jar cloud_a3/Receiver/CloudAssignment3.jar
 
